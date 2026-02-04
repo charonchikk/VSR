1e2f8942bcd7ab5dfd7cdef7dc7cb84fe6303bc3
Kirill <offcbet803@gmail.com>
2026-02-04 20:24:41 +0400

    vsrPush

--- Contracts/contracts/ProfiTC.sol
+++ Contracts/contracts/ProfiTC.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Professional is ERC20, ERC20Votes {
-    constructor(string memory name, string memory symbol) ERC20 ("Professional", "PROFI") EIP712(name, "1") {
-        _mint(msg.sender, 100000 *10 **12);
+contract ProfessionalToken is ERC20 {
+
+    constructor(address[] memory members)
+        ERC20("Professional", "PROFI")
+    {
+        uint256 total = 100000 * 10 ** 12;
+        uint256 perMember = total / members.length;
+
+        for (uint i = 0; i < members.length; i++) {
+            _mint(members[i], perMember);
+        }
     }
-    
-    function _update(address from, address to, uint amount) internal override(ERC20,ERC20Votes) {
-        super._update(from, to, amount);
-    }
 
-    function decimals() public view override returns (uint8) {
+    function decimals() public pure override returns (uint8) {
         return 12;
     }
 }
+
